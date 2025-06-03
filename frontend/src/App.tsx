4641870b19eda07ac625936181288c42600169d3
Stanley Ching <stanley.ching@student.manchester.ac.uk>
2025-06-03 22:15:00 +0100

    Initialise tech stacks and directory

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+    </div>
+  );
+}
+export default App;
